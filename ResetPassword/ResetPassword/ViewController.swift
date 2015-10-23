b9a31585315c9c9d60fda98a2629d8c964cd2385
Satish Chauhan <satishchauhan80@gmail.com>
2015-10-23 12:39:46 +0530

    Demo website and iOS project

---
+++ ResetPassword/ResetPassword/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  ResetPassword
+//
+//  Created by Satish Chauhan on 16/10/15.
+//  Copyright (c) 2015 Vinsol. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+}
+
---
+++ ResetPassword/ResetPassword/bridge.h
@@ -0,0 +1,14 @@
+//
+//  bridge.h
+//  
+//
+//  Created by Satish Chauhan on 16/10/15.
+//
+//
+
+#ifndef _bridge_h
+#define _bridge_h
+
+#import "JLRoutes.h"
+
+#endif
